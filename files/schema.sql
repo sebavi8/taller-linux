fe380a99107d4e43f7401a40ca76947e3adca2ca
sebavi8 <sebavi8@hotmail.com>
2026-08-09 15:10:05 -0300

    Update schema.sql

--- files/schema.sql
+++ files/schema.sql
@@ -3,11 +3,11 @@ USE cumples;
 
 CREATE TABLE IF NOT EXISTS cumpleanios (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
-  fecha DATE
+  nombre VARCHAR(100) NOT NULL UNIQUE,
+  fecha DATE NOT NULL
 );
 
-INSERT INTO cumpleanios (nombre, fecha) VALUES
+INSERT IGNORE INTO cumpleanios (nombre, fecha) VALUES
 ('Frodo Baggins', '2005-01-14'),
 ('Aragorn', '2004-02-09'),
 ('Arwen Undomiel', '1994-12-09');
