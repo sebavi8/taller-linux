716241976c8b50dfe4c54976244bfd2d6c21fcaa
sebavi8 <sebavi8@hotmail.com>
2026-08-09 15:48:02 -0300

    Update schema.sql

--- files/schema.sql
+++ files/schema.sql
@@ -6,8 +6,3 @@ CREATE TABLE IF NOT EXISTS cumpleanios (
   nombre VARCHAR(100) NOT NULL UNIQUE,
   fecha DATE NOT NULL
 );
-
-INSERT IGNORE INTO cumpleanios (nombre, fecha) VALUES
-('Frodo Baggins', '2005-01-14'),
-('Aragorn', '2004-02-09'),
-('Arwen Undomiel', '1994-12-09');
